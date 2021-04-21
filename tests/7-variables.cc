0ef00432ce2201a7de2b4acea8ea328323521d36
Noah Frontzkowski <noah.frontzkowski@tuhh.de>
2021-04-21 19:00:19 +0200

    added test for constinit

---
+++ tests/7-variables.cc
@@ -0,0 +1,16 @@
+/******************************************************************************
+                     Compiler Construction - Project 1
+                        Test for Variable Qualifieres
+*******************************************************************************/
+
+#include <iostream>
+
+using std;
+
+
+int main()
+{
+    // should fail
+    constinit const int;
+    return 0;
+}
