c9d3c1192ff01864fd405dbfeb2c0683b52c8f65
Leonard Schroeter <leonard.schroeter@tuhh.de>
2021-04-22 20:53:51 +0200

    Add test which should fail

--- tests/7-variables.cc
+++ tests/7-variables.cc
@@ -12,5 +12,7 @@ int main()
 {
     // should parse according to DOMjudge test 68
     const constinit int uninitialized;
+    // should fail, but doesn't
+    int & & & & & multipleRefOperators;
     return 0;
 }
